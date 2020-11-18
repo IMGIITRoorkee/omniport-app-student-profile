a527b5cc3ec776436707e5efe936ea0217e0a06f
Drumil Patel <drumilpatel720@gmail.com>
2020-11-19 00:17:12 +0530

    Fix linting issues
    Signed-off-by: Drumil Patel <drumilpatel720@gmail.com>

--- http_urls.py
+++ http_urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import url, include
 from rest_framework import routers
 import inflection
 
-from student_profile.views import SocialLinkViewSet, DragAndDropView, PublishPageView, StudentSearchList
+from student_profile.views import SocialLinkViewSet, DragAndDropView, \
+    PublishPageView, StudentSearchList
 from student_profile.serializers.generic_serializers import common_dict
 
 app_name = 'student_profile'
@@ -16,7 +17,7 @@ for model in common_dict:
         basename=model,
     )
 
-router.register(r'social_link',SocialLinkViewSet,basename="SocialLink")
+router.register(r'social_link', SocialLinkViewSet, basename="SocialLink")
 
 urlpatterns = [
     url(r'publish', PublishPageView.as_view()),

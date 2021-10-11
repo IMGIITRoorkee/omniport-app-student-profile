f213f00296139fbe0e1d4a759f8b10e8eeb16e52
root <root@channeli.in>
2021-10-11 19:02:14 +0530

    Revert "Rectify import according to new django"
    This reverts commit 106db2a6ba22e4b976090b44e4bf3e76c9830678.

--- views.py
+++ views.py
@@ -7,7 +7,6 @@ from django.core.exceptions import (
     ObjectDoesNotExist, 
     ValidationError, 
     ImproperlyConfigured,
-    FieldDoesNotExist
 )
 from django.utils.datastructures import MultiValueDictKeyError
 
